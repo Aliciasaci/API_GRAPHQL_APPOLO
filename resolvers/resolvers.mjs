bf0582af2e845253e3fc3dcd1292a7dfe1641221
Alicia Saci <lyciiasci@gmail.com>
2023-11-29 21:48:26 +0100

    update

--- resolvers/resolvers.mjs
+++ resolvers/resolvers.mjs
@@ -1,5 +1,25 @@
 import pool from '../database/db.mjs';
 
+const getFilmsByDirectorId = async (directorId) => {
+    try {
+        const result = await pool.query('SELECT * FROM "films" WHERE director_id = $1', [directorId]);
+        return result.rows;
+    } catch (error) {
+        console.error('Error querying films by director ID:', error);
+        throw error;
+    }
+};
+
+const getUserById = async (userId) => {
+    try {
+        const result = await pool.query('SELECT * FROM "users" WHERE id = $1', [userId]);
+        return result.rows[0];
+    } catch (error) {
+        console.error('Error querying user by ID:', error);
+        throw error;
+    }
+};
+
 export const resolvers = {
     Query: {
         users: async () => {
@@ -8,8 +28,23 @@ export const resolvers = {
                 return result.rows;
             } catch (error) {
                 console.error('Error querying users:', error);
-                throw error; // Rethrow the error for proper handling in your application
+                throw error;
             }
         },
+        films: async () => {
+            try {
+                const result = await pool.query('SELECT * FROM "films"');
+                return result.rows;
+            } catch (error) {
+                console.error('Error querying films:', error);
+                throw error;
+            }
+        },
+    },
+    User: {
+        films: (user) => getFilmsByDirectorId(user.id),
+    },
+    Film: {
+        director: (film) => getUserById(film.director_id),
     },
 };
--- schemas/schema.mjs
+++ schemas/schema.mjs
@@ -5,9 +5,18 @@ type User {
   id: ID!
   firstname: String!
   email: String!
+  films: [Film]
+}
+
+type Film {
+  id : ID!
+  title : String!
+  director_id : Int!
+  director: User
 }
 
 type Query {
-  users: [User]
+  users: [User],
+  films: [Film]
 }
 `;
